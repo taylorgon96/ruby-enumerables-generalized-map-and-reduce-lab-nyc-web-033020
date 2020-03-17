30027acb5040ed07650438726ab04fbdeb986a17
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-17 14:22:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,4 @@
 # Your Code Here
 def map
-  [1, 2, 3, -9].map
+  [1, 2, 3, -9].map {|n| n * -1}
 end 
-describe 'my own map' do
-  it "returns an array with all values made negative" do
-    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
-  end
